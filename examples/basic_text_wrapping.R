72efc1d287bfd953c49ce133168cab4bb22e5408
Claus Wilke <wilke@austin.utexas.edu>
2019-07-02 16:06:53 -0500

    updated examples of basic text wrapping

--- examples/basic_text_wrapping.R
+++ examples/basic_text_wrapping.R
@@ -2,32 +2,24 @@ library(grid)
 library(gridtext)
 
 wrap_grob <- function(text, x = unit(0.2, "npc"), y = unit(0.8, "npc"), width = unit(0.5, "npc"),
-                      gp = gpar(), linespacing_pt = 14, render_cpp = TRUE) {
+                      gp = gpar(), render_cpp = TRUE) {
   # currently ignores newlines
   tokens <- stringr::str_split(text, "[[:space:]]+")[[1]]
 
   grobs <- lapply(tokens, gridtext:::text_grob, gp = gp)
   widths_pt <- vapply(grobs, grob_width_pt, numeric(1))
   sp_grob <- gridtext:::text_grob(" ", gp = gp)
-  space_width_pt <- grob_width_pt(sp_grob)
-
-  if (isTRUE(render_cpp)) {
-    nodes <- mapply(gridtext:::bl_make_grob_box, grobs, widths_pt)
-    bl_nodes <- gridtext:::bl_make_node_list(nodes)
-    hbox <- gridtext:::bl_make_hbox(bl_nodes, linespacing_pt, space_width_pt)
-  } else {
-    hbox <- NULL
-  }
+  td <- text_details("a", gp = gp)
 
   gTree(
     x = x,
     y = y,
     width = width,
+    tokens = tokens,
     grobs = do.call(gList, grobs),
     widths_pt = widths_pt,
-    space_width_pt = space_width_pt,
-    linespacing_pt = linespacing_pt,
-    hbox = hbox,
+    space_width_pt = td$space_pt,
+    linespacing_pt = 1.2*(td$ascent_pt + td$descent_pt),
     render_cpp = render_cpp,
     gp = gp,
     cl = "wrap_grob"
@@ -42,8 +34,7 @@ makeContent.wrap_grob <- function(x) {
 
   if (isTRUE(x$render_cpp)) {
     print("rendering via C++")
-    gridtext:::bl_calc_layout(x$hbox, width_pt)
-    children <- gridtext:::bl_render(x$hbox, x_pt, y_pt)
+    children <- gridtext:::test_hbox(tokens, width_pt, x_pt, y_pt, x$gp)
   } else {
     print("rendering via R")
     # x and y offsets as we draw
@@ -76,12 +67,29 @@ text <- "The quick brown fox jumps over the lazy dog. Lorem ipsum dolor sit amet
   fermentum odio id, facilisis velit."
 
 
-g1 <- wrap_grob(text, gp = gpar(fontsize = 14), linespacing_pt = 17, render_cpp = FALSE)
+g1 <- wrap_grob(text, gp = gpar(fontsize = 14), render_cpp = FALSE)
 g2 <- wrap_grob(
   text,
-  x = unit(0.201, "npc"), y = unit(0.801, "npc"),
-  gp = gpar(fontsize = 14, col = "red"), linespacing_pt = 17, render_cpp = TRUE
+  x = unit(0.2015, "npc"), y = unit(0.8015, "npc"),
+  gp = gpar(fontsize = 14, col = "red"), render_cpp = TRUE
 )
 grid.newpage()
 grid.draw(g1)
 grid.draw(g2)
+
+# benchmark shows layouting via C++ is about 10 times faster
+# than just grob generation in regular R
+
+tokens <- stringr::str_split(text, "[[:space:]]+")[[1]]
+gp <- gpar(fontfamily = "Times", fontsize = 10, col = "blue")
+f_cpp <- function(tokens, gp) {
+  gridtext:::test_hbox(tokens, 200, 20, 400, gp)
+}
+
+f_R <- function(tokens, gp) {
+  grobs <- lapply(tokens, textGrob, gp = gp)
+  widths_pt <- vapply(grobs, grob_width_pt, numeric(1))
+}
+
+microbenchmark::microbenchmark(f_cpp(tokens, gp), f_R(tokens, gp))
+
