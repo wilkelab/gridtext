003c083672e488223ab1db26e670c5d094c19e67
Claus Wilke <wilke@austin.utexas.edu>
2019-07-01 21:15:18 -0500

    New function text_details() with improved size calculations.

---
+++ R/text-details.R
@@ -0,0 +1,95 @@
+#' Calculate text details for a given text label
+#'
+#' Calculate text details for a given text label
+#' @param label Character vector containing the label. Can handle only one label at a time.
+#' @param gp Grid graphical parameters defining the font (`fontfamily`, `fontface`, and
+#'   `fontface` should be defined).
+#' @examples
+#' text_details("Hello world!", gpar(fontfamily = "", fontface = "plain", fontsize = 12))
+#' text_details("Hello world!", gpar(fontfamily = "", fontface = "plain", fontsize = 24))
+#' text_details(
+#'   "Hello world\nwith newline",
+#'   gpar(fontfamily = "", fontface = "plain", fontsize = 12)
+#' )
+#' @export
+text_details <- function(label, gp = gpar()) {
+  fontfamily <- gp$fontfamily %||% grid::get.gpar("fontfamily")$fontfamily
+  fontface <- gp$fontface %||% grid::get.gpar("fontface")$fontface
+  fontsize <- gp$fontsize %||% grid::get.gpar("fontsize")$fontsize
+  fontkey <- paste0(fontfamily, fontface, fontsize)
+
+  if (length(fontkey) != 1 || length(label) != 1) {
+    stop("Function `text_details()` is not vectorized.", call. = FALSE)
+  }
+
+  # ascent and width depend on label and font
+  l1 <- text_info(label, fontkey, fontfamily, fontface, fontsize)
+  # descent and space width depend only on font
+  l2 <- font_info(fontkey, fontfamily, fontface, fontsize)
+
+  # concatenate, result is a list with four members, width_pt, ascent_pt, descent_pt, space_pt
+  c(l1, l2)
+}
+
+font_info_cache <- new.env(parent = emptyenv())
+font_info <- function(fontkey, fontfamily, fontface, fontsize) {
+  info <- font_info_cache[[fontkey]]
+
+  if (is.null(info)) {
+    descent_pt <- convertHeight(grobDescent(textGrob(
+      label = "gjpqyQ",
+      gp = gpar(
+        fontsize = fontsize,
+        fontfamily = fontfamily,
+        fontface = fontface,
+        cex = 1
+      )
+    )), "pt", valueOnly = TRUE)
+
+    space_pt <- convertWidth(grobWidth(textGrob(
+      label = " ",
+      gp = gpar(
+        fontsize = fontsize,
+        fontfamily = fontfamily,
+        fontface = fontface,
+        cex = 1
+      )
+    )), "pt", valueOnly = TRUE)
+
+    info <- list(descent_pt = descent_pt, space_pt = space_pt)
+    font_info_cache[[fontkey]] <- info
+  }
+  info
+}
+
+text_info_cache <- new.env(parent = emptyenv())
+text_info <- function(label, fontkey, fontfamily, fontface, fontsize) {
+  key <- paste0(label, fontkey)
+  info <- text_info_cache[[key]]
+
+  if (is.null(info)) {
+    ascent_pt <- convertHeight(grobHeight(textGrob(
+      label = label,
+      gp = gpar(
+        fontsize = fontsize,
+        fontfamily = fontfamily,
+        fontface = fontface,
+        cex = 1
+      )
+    )), "pt", valueOnly = TRUE)
+
+    width_pt <- convertWidth(grobWidth(textGrob(
+      label = label,
+      gp = gpar(
+        fontsize = fontsize,
+        fontfamily = fontfamily,
+        fontface = fontface,
+        cex = 1
+      )
+    )), "pt", valueOnly = TRUE)
+
+    info <- list(width_pt = width_pt, ascent_pt = ascent_pt)
+    text_info_cache[[key]] <- info
+  }
+  info
+}
---
+++ tests/testthat/test-text-details.R
@@ -0,0 +1,39 @@
+context("text details")
+
+test_that("text_details() calculates info correctly", {
+  # descent and space are independent of string
+  gp1 <- gpar(fontfamily = "Helvetica", fontface = "plain", fontsize = 10)
+  t1 <- text_details("abcd", gp = gp1)
+  t2 <- text_details("gjqp", gp = gp1)
+  expect_equal(t1$descent_pt, t2$descent_pt)
+  expect_equal(t1$space_pt, t2$space_pt)
+
+  # recalculating the same details gives same results (tests caching)
+  t2 <- text_details("abcd", gp = gp1)
+  expect_equal(t1$width_pt, t2$width_pt)
+  expect_equal(t1$ascent_pt, t2$ascent_pt)
+  expect_equal(t1$descent_pt, t2$descent_pt)
+  expect_equal(t1$space_pt, t2$space_pt)
+
+  # all parameters scale with font size
+  gp2 <- gpar(fontfamily = "Helvetica", fontface = "plain", fontsize = 20)
+  t2 <- text_details("abcd", gp = gp2)
+  expect_equal(2 * t1$width_pt, t2$width_pt)
+  expect_equal(2 * t1$ascent_pt, t2$ascent_pt)
+  expect_equal(2 * t1$descent_pt, t2$descent_pt)
+  expect_equal(2 * t1$space_pt, t2$space_pt)
+
+  # parameters change with font
+  gp2 <- gpar(fontfamily = "Times", fontface = "plain", fontsize = 10)
+  t2 <- text_details("abcd", gp = gp2)
+  expect_false(t1$width_pt == t2$width_pt)
+  expect_false(t1$ascent_pt == t2$ascent_pt)
+  expect_false(t1$descent_pt == t2$descent_pt)
+  expect_false(t1$space_pt == t2$space_pt)
+
+  # font details are identical to what we would get from an actual grob
+  g <- textGrob("Qbcd", gp = gp1)
+  t1 <- text_details("Qbcd", gp = gp1)
+  expect_equal(t1$ascent_pt, convertHeight(grobHeight(g), "pt", valueOnly = TRUE))
+  expect_equal(t1$width_pt, convertWidth(grobWidth(g), "pt", valueOnly = TRUE))
+})
