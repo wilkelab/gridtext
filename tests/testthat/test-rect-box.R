3a26dd83b763e735f1b31371ce35048326211c5f
Claus Wilke <wilke@austin.utexas.edu>
2019-07-05 17:10:56 -0500

    tests for a few missing cases

--- tests/testthat/test-rect-box.R
+++ tests/testthat/test-rect-box.R
@@ -130,4 +130,63 @@ test_that("size policies", {
   inner <- g[[2]]
   expect_identical(inner$x, unit(100 + 8 + 128, "pt"))
   expect_identical(inner$y, unit(200 + 600 - 1 - 16 - 10, "pt"))
+
+  rb <- bl_make_rect_box(
+    cb, 400, 600, c(1, 2, 4, 8), c(16, 32, 64, 128), gp = gpar(),
+    width_policy = "fixed", height_policy = "native"
+  )
+  bl_calc_layout(rb, 50, 300)
+  g <- bl_render(rb, 100, 200)
+
+  outer <- g[[1]]
+  expect_identical(outer$x, unit(100 + 8, "pt"))
+  expect_identical(outer$y, unit(200 + 4, "pt"))
+  expect_identical(outer$width, unit(400 - 2 - 8, "pt"))
+  expect_identical(outer$height, unit(10 + 16 + 64, "pt"))
+  inner <- g[[2]]
+  expect_identical(inner$x, unit(100 + 8 + 128, "pt"))
+  expect_identical(inner$y, unit(200 + 4 + 64, "pt"))
+
+
+  # native size policies with no content
+  rb <- bl_make_rect_box(
+    nb, 400, 600, c(1, 2, 4, 8), c(16, 32, 64, 128), gp = gpar(),
+    width_policy = "native", height_policy = "native"
+  )
+  bl_calc_layout(rb, 50, 300)
+  g <- bl_render(rb, 100, 200)
+
+  outer <- g[[1]]
+  expect_identical(outer$x, unit(100 + 8, "pt"))
+  expect_identical(outer$y, unit(200 + 4, "pt"))
+  # native width/height now depends only on padding, since content size is 0
+  expect_identical(outer$width, unit(32 + 128, "pt"))
+  expect_identical(outer$height, unit(16 + 64, "pt"))
+
+  rb <- bl_make_rect_box(
+    nb, 400, 600, c(1, 2, 4, 8), c(16, 32, 64, 128), gp = gpar(),
+    width_policy = "native", height_policy = "fixed"
+  )
+  bl_calc_layout(rb, 50, 300)
+  g <- bl_render(rb, 100, 200)
+
+  outer <- g[[1]]
+  expect_identical(outer$x, unit(100 + 8, "pt"))
+  expect_identical(outer$y, unit(200 + 4, "pt"))
+  expect_identical(outer$width, unit(32 + 128, "pt"))
+  expect_identical(outer$height, unit(600 - 1 - 4, "pt"))
+
+  rb <- bl_make_rect_box(
+    nb, 400, 600, c(1, 2, 4, 8), c(16, 32, 64, 128), gp = gpar(),
+    width_policy = "fixed", height_policy = "native"
+  )
+  bl_calc_layout(rb, 50, 300)
+  g <- bl_render(rb, 100, 200)
+
+  outer <- g[[1]]
+  expect_identical(outer$x, unit(100 + 8, "pt"))
+  expect_identical(outer$y, unit(200 + 4, "pt"))
+  expect_identical(outer$width, unit(400 - 2 - 8, "pt"))
+  expect_identical(outer$height, unit(16 + 64, "pt"))
+
 })
