569eb26bfd82f0fa17feee1977f8870ec2fca53b
Claus Wilke <wilke@austin.utexas.edu>
2019-08-01 23:50:25 -0500

    linebreaker compiles, most basic of tests passes

--- src/line-breaker.h
+++ src/line-breaker.h
@@ -7,6 +7,7 @@ using namespace Rcpp;
 #include <list>
 #include <cmath>
 #include <algorithm>
+#include <memory>
 using namespace std;
 
 #include "layout.h"
@@ -17,6 +18,24 @@ using namespace std;
 /* The LineBreaker class implements the Knuth-Plass algorithm, as described in
  * Knuth & Plass 1981
  */
+
+// support class representing an active breakpoint
+struct Breakpoint {
+  size_t position, line;
+  int fitness_class;
+  Length totalwidth, totalstretch, totalshrink;
+  int demerits;
+  Breakpoint* previous;
+
+  Breakpoint(size_t _position, size_t _line, int _fitness_class,
+             Length _totalwidth, Length _totalstretch, Length _totalshrink,
+             int _demerits, Breakpoint* _previous = nullptr) :
+    position(_position), line(_line), fitness_class(_fitness_class),
+    totalwidth(_totalwidth), totalstretch(_totalstretch),
+    totalshrink(_totalshrink), demerits(_demerits), previous(_previous)
+  {};
+};
+
 
 template <class Renderer>
 class LineBreaker {
@@ -36,45 +55,31 @@ private:
   // sums of widths, stretch, and shrink
   vector<Length> m_sum_widths, m_sum_stretch, m_sum_shrink;
 
-  // internal class representing an active breakpoint
-  struct Breakpoint {
-    size_t position, line;
-    int fitness_class;
-    Length totalwidth, totalstretch, totalshrink;
-    int demerits;
-    Breakpoint* previous;
+  // active and passive nodes
+  list<unique_ptr<Breakpoint>> m_active_nodes;
+  vector<unique_ptr<Breakpoint>> m_passive_nodes;
 
-    Breakpoint(size_t _position, size_t _line, int _fitness_class,
-               Length _totalwidth, Length _totalstretch, Length _totalshrink,
-               int _demerits, Breakpoint* _previous = nullptr) :
-      position(_position), line(_line), fitness_class(_fitness_class),
-      totalwidth(_totalwidth), totalstretch(_totalstretch),
-      totalshrink(_totalshrink), demerits(_demerits), previous(_previous)
-    {};
-  };
 
-  using BreakpointList = list<Breakpoint>;
-  using BreaksList = vector<size_t>;
-
-  void add_active_node(BreakpointList &active_nodes, const Breakpoint &node) {
+  void add_active_node(unique_ptr<Breakpoint> node) {
     // find the first position at which the line number of the new node
     // exceeds the line number of the node in the list
-    auto i_node = active_nodes.begin();
-    while (i_node != active_nodes.end() && i_node->line < node.line) {
+    auto i_node = m_active_nodes.begin();
+    while (i_node != m_active_nodes.end() && (*i_node)->line < node->line) {
       i_node++;
     }
     auto i_node_insert = i_node; // store insertion point
 
     // now check if there's another node with the same line number,
     // position, and fitness; if yes, drop the new node
-    while (i_node != active_nodes.end() && i_node->line == node.line) {
-      if (i_node->fitness_class == node.fitness_class && i_node->position == node.position) {
+    while (i_node != m_active_nodes.end() && (*i_node)->line == node->line) {
+      if ((*i_node)->fitness_class == node->fitness_class &&
+          (*i_node)->position == node->position) {
         return;
       }
       i_node++;
     }
 
-    active_nodes.insert(i_node_insert, node);
+    m_active_nodes.insert(i_node_insert, node);
   }
 
 
@@ -150,7 +155,7 @@ private:
       if (shrink > 0) {
         r = (len_avail - len)/shrink;
       } else {
-        r = Glue<Renderer>::infinity; // TODO: Should this be -infinity?
+        r = -1*Glue<Renderer>::infinity;
       }
     }
     // r = 0 if len == len_avail
@@ -169,19 +174,19 @@ private:
     return fitness_class;
   }
 
-  double compute_demerits(double p, double r, bool forced_break, bool f_i, bool f_a, int fitness_class, int fitness_class_active) {
+  double compute_demerits(size_t i, size_t i_active, double r, int fitness_class, int fitness_class_active) {
     double demerits;
 
-    if (p >= 0) {
-      demerits = pow(1 + 100 * pow(abs(r), 3) + p, 3);
-    } else if (forced_break) {
-      demerits = pow(1 + 100 * pow(abs(r), 3), 2) - pow(p, 2);
+    if (m_p[i] >= 0) {
+      demerits = pow(1 + 100 * pow(abs(r), 3) + m_p[i], 3);
+    } else if (is_forced_break(i)) {
+      demerits = pow(1 + 100 * pow(abs(r), 3), 2) - pow(m_p[i], 2);
     } else {
       demerits = pow(1 + 100 * pow(abs(r), 3), 2);
     }
 
     // adjust demeteris for flagged items
-    demerits = demerits + (m_flagged_demerit * f_i * f_a);
+    demerits = demerits + (m_flagged_demerit * m_f[i] * m_f[i_active]);
 
     // add demerits for changes in fitness class
     if (abs(fitness_class - fitness_class_active) > 1) {
@@ -191,12 +196,17 @@ private:
     return demerits;
   }
 
+  // to write unit tests that have access to private members
+  friend class TestLineBreaker;
 
 public:
   LineBreaker(const BoxList<Renderer>& nodes, const vector<Length> &line_lengths, double tolerance = 1,
               double fitness_demerit = 100, double flagged_demerit = 100) :
     m_nodes(nodes), m_line_lengths(line_lengths), m_tolerance(tolerance), m_fitness_demerit(fitness_demerit),
     m_flagged_demerit(flagged_demerit) {
+    // tolerance is \rho in the paper
+    // fitness_demerit is \gamma in the paper
+
     m_m = nodes.size();
 
     // set up vectors with the five possible values (w, y, z, p, f)
@@ -208,17 +218,17 @@ public:
     m_f.reserve(m_m);
 
     for (auto i_node = m_nodes.begin(); i_node != m_nodes.end(); i_node++) {
-      m_w.push_back(i_node->width());
-      if (i_node->type() == NodeType::glue) {
-        m_y.push_back(static_cast<Glue<Renderer>*>(i_node)->stretch());
-        m_z.push_back(static_cast<Glue<Renderer>*>(i_node)->shrink());
+      m_w.push_back((*i_node)->width());
+      if ((*i_node)->type() == NodeType::glue) {
+        m_y.push_back(static_cast<Glue<Renderer>*>(i_node->get())->stretch());
+        m_z.push_back(static_cast<Glue<Renderer>*>(i_node->get())->shrink());
         m_p.push_back(0);
         m_f.push_back(false);
-      } else if (i_node->type() == NodeType::penalty) {
+      } else if ((*i_node)->type() == NodeType::penalty) {
         m_y.push_back(0);
         m_z.push_back(0);
-        m_p.push_back(static_cast<Penalty<Renderer>*>(i_node)->penalty());
-        m_f.push_back(static_cast<Penalty<Renderer>*>(i_node)->flagged());
+        m_p.push_back(static_cast<Penalty<Renderer>*>(i_node->get())->penalty());
+        m_f.push_back(static_cast<Penalty<Renderer>*>(i_node->get())->flagged());
       } else {
         m_y.push_back(0);
         m_z.push_back(0);
@@ -247,95 +257,94 @@ public:
 
 
   // TODO: Do we really want to return a vector? Probably not.
-  BreaksList compute_breaks() {
-    // tolerance is \rho in the paper
-    // fitness_demerit is \gamma in the paper
+  void compute_breaks(vector<size_t> &final_breaks) {
+    // we return the final breaks via this argument;
+    // clear this vector to make sure we're starting from a clean slate
+    final_breaks.clear();
 
     // if there are no nodes we have no breaks
     if (m_m == 0) {
-      return BreaksList();
+      return;
     }
 
     // set up list of active nodes, initialize with
     // break at beginning of text
-    BreakpointList active_nodes;
-    active_nodes.emplace_back(0, 0, 1, 0, 0, 0, 0);
+    m_active_nodes.clear();
+    m_passive_nodes.clear();
+    auto ptr = unique_ptr<Breakpoint>(new Breakpoint(0, 0, 1, 0, 0, 0, 0));
+    m_active_nodes.push_back(ptr);
 
     for (size_t i = 0; i < m_m; i++) {
       // we can only break at feasible breakpoints
       if (is_feasible_breakpoint(i)) {
-        BreaksList breaks; // list of new possible breakpoints
+        vector<unique_ptr<Breakpoint>> breaks; // list of new possible breakpoints
 
         // iterate over all currently active nodes and evaluate breaking
         // between there and i
 
         // need to use a while loop because we modify the list as we iterate
-        auto i_active = active_nodes.begin();
-        while (i_active != active_nodes.end()) {
-          double r = compute_adjustment_ratio(i_active->position, i, i_active->line);
+        auto i_active = m_active_nodes.begin();
+        while (i_active != m_active_nodes.end()) {
+          double r = compute_adjustment_ratio((*i_active)->position, i, (*i_active)->line);
 
           // remove active nodes when forced break or overfull line
           if (r < -1 || is_forced_break(i)) {
-            // TODO: We need to keep track of the last removed node, since we may have to restore it at the end
-            i_active = active_nodes.erase(i_active); // this advances the iterator
+            m_passive_nodes.push_back(*i_active);
+            i_active = m_active_nodes.erase(i_active); // this advances the iterator
             continue;
           }
 
           if (-1 <= r <= m_tolerance) {
             int fitness_class = compute_fitness_class(r);
-            double demerits = compute_demerits(
-              m_p[i], r, is_forced_break(i), m_f[i], m_f[i_active->position],
-              fitness_class, i_active->fitness_class
-            );
+            double demerits = compute_demerits(i, (*i_active)->position, r, fitness_class, (*i_active)->fitness_class);
 
             // record feasible break from A to i
-            // TODO: There is a type mismatch here. breaks is a vector of size_t, not of Breakpoint.
-            breaks.emplace_back(
-              i, i_active->line + 1, fitness_class,
-              m_sum_widths[i], m_sum_stretch[i], m_sum_shrink[i],
-              demerits
+            auto ptr = unique_ptr<Breakpoint>(
+              new Breakpoint(
+                i, i_active->line + 1, fitness_class,
+                m_sum_widths[i], m_sum_stretch[i], m_sum_shrink[i],
+                demerits
+              )
             );
+            breaks.push_back(ptr);
+
           }
           i_active++;
         }
         // add all the new breaks to the list of active nodes
         for (auto i_brk = breaks.begin(); i_brk != breaks.end(); i_brk++) {
-          add_active_node(active_nodes, *i_brk);
+          add_active_node(*i_brk);
         }
+        breaks.clear();
       }
 
       // find the active node with the lowest number of demerits
       // TODO: handle empty list correctly
       // If the list is empty, we restore the last removed node and a node at the current
       // breakpoint
-      auto i_active = active_nodes.begin();
+      auto i_active = m_active_nodes.begin();
       double min_demerits = i_active->demerits;
       auto i_min = i_active;
       while (true) {
         // this assumes there is at least one node in the list
         i_active++;
-        if (i_active == active_nodes.end()) {
+        if (i_active == m_active_nodes.end()) {
           break;
         }
-        if (i_active->demerits < min_demerits) {
-          min_demerits = i_active->demerits;
+        if ((*i_active)->demerits < min_demerits) {
+          min_demerits = (*i_active)->demerits;
           i_min = i_active;
         }
       }
 
       // now build a list of break points going backwards from minimum
       // demerits node to beginning
-      BreaksList final_breaks;
-      Breakpoint *p_node = i_min;
+      Breakpoint *p_node = *i_min;
       while (p_node != nullptr) {
         final_breaks.push_back(p_node->position);
         p_node = p_node->previous;
       }
       reverse(final_breaks.begin(), final_breaks.end());
-
-      // TODO: Where do we keep track of the final r for each line?
-      // Currently it looks like we're throwing it away.
-      return final_breaks;
     }
   }
 };
---
+++ src/test-line-breaker.cpp
@@ -0,0 +1,32 @@
+#include <testthat.h>
+
+#include <iostream>
+
+#include "layout.h"
+#include "null-box.h"
+#include "glue.h"
+#include "grid-renderer.h"
+#include "line-breaker.h"
+
+class TestLineBreaker {
+public:
+  static bool test1() {
+    BoxPtr<GridRenderer> b1(new Glue<GridRenderer>(5));
+    BoxPtr<GridRenderer> b2(new NullBox<GridRenderer>(3));
+
+    BoxList<GridRenderer> l;
+    l.push_back(b1);
+    l.push_back(b2);
+
+    vector<Length> line_lengths = {20};
+    LineBreaker<GridRenderer> lb(l, line_lengths);
+
+    return (lb.m_nodes.size() == 2);
+  }
+};
+
+context("Line breaker") {
+  test_that("Create linebreaker class") {
+    expect_true(TestLineBreaker::test1());
+  }
+}
