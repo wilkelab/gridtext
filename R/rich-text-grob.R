0ead28841b9244728bba73505d312f0a20ed8029
Claus Wilke <wilke@austin.utexas.edu>
2019-08-07 12:55:39 -0500

    basic structure for rich_text_grob mirroring text_grob.

--- R/rich-text-grob.R
+++ R/rich-text-grob.R
@@ -10,84 +10,85 @@
 #' @param use_markdown Should the `text` input be treated as markdown?
 #' @examples
 #' library(grid)
+#'
+#' text <- c(
+#'   "Some text **in bold.**", "Linebreaks<br>Linebreaks<br>Linebreaks",
+#'   "*x*<sup>2</sup> + 5*x* + *C*<sub>i</sub>",
+#'   "Mixing **it** all *up,<br> for good measure.*"
+#' )
+#'
+#' x <- c(.2, .6, .2, .5)
+#' y <- c(.8, .8, .2, .4)
+#' rot <- c(0, 0, 45, -45)
+#'
 #' grid.newpage()
-#' grid.draw(rich_text_grob("Some text **in bold.**"))
+#' g <- rich_text_grob(text, x, y, rot = rot)
+#' grid.draw(g)
+#' grid.points(x, y, default.units = "npc")
 #' @export
-rich_text_grob <- function(text, width = NULL, height = NULL,
-                           x = unit(0.5, "npc"), y = unit(0.5, "npc"),
-                           hjust = 0.5, vjust = 0.5, gp = gpar(), use_markdown = TRUE) {
+rich_text_grob <- function(text, x = unit(0.5, "npc"), y = unit(0.5, "npc"),
+                           hjust = 0.5, vjust = 0.5, rot = 0, default.units = "npc",
+                           name = NULL, gp = gpar(), vp = NULL, use_markdown = TRUE) {
+  # make sure x and y are units
+  if (!is.unit(x))
+    x <- unit(x, default.units)
+  if (!is.unit(y))
+    y <- unit(y, default.units)
+
+  # make sure all input has same length
+  n <- unique(length(text), length(x), length(y))
+  if (length(n) > 1) {
+    stop("Arguments `text`, `x`, and `y` must have the same length.", call. = FALSE)
+  }
+  hjust <- rep_len(hjust, n)
+  vjust <- rep_len(vjust, n)
+  rot <- rep_len(rot, n)
+  use_markdown <- rep_len(use_markdown, n)
+  gp_list <- rep_len(list(gp), n)
+
+  grobs <- mapply(
+    make_rich_text_grob,
+    text,
+    x,
+    y,
+    hjust,
+    vjust,
+    rot,
+    use_markdown,
+    gp_list,
+    SIMPLIFY = FALSE
+  )
+
+  children <- do.call(gList, grobs)
+
+  gTree(
+    gp = gp,
+    vp = vp,
+    name = name,
+    children = children,
+    cl = "rich_text_grob"
+  )
+}
+
+make_rich_text_grob <- function(text, x, y, hjust, vjust, rot, use_markdown, gp_list) {
   if (use_markdown) {
     text <- markdown::markdownToHTML(text = text, options = c("use_xhtml", "fragment_only"))
   }
   doctree <- read_html(text)
 
-  drawing_context <- setup_context(gp = gp)
-  boxlist <- process_tags(xml2::as_list(doctree)$html$body, drawing_context)
-  if (is.null(width)) {
-    vbox <- bl_make_vbox(boxlist, width_pt = 0, hjust = hjust, vjust = vjust, width_policy = "native")
+  if (length(gp_list) > 0) {
+    # TODO: doesn't work yet, setup_context breaks
+    drawing_context <- setup_context(gp = gp_list[[1]])
   } else {
-    vbox <- bl_make_vbox(boxlist, width_pt = 100, hjust = hjust, vjust = vjust, width_policy = "relative")
+    drawing_context <- setup_context(gp = NULL)
   }
+  boxlist <- process_tags(xml2::as_list(doctree)$html$body, drawing_context)
+  vbox <- bl_make_vbox(boxlist, width_pt = 0, hjust = hjust, vjust = vjust, width_policy = "native")
+  bl_calc_layout(vbox, 0, 0)
+  grobs <- bl_render(vbox, 0, 0)
 
   gTree(
-    width = width,
-    height = height,
-    x = x,
-    y = y,
-    hjust = hjust,
-    vjust = vjust,
-    vbox = vbox,
-    gp = gp,
-    cl = "rich_text_grob"
+    children = grobs,
+    vp = viewport(x = x, y = y, just = c(0, 0), angle = rot)
   )
-}
-
-#' @export
-makeContent.rich_text_grob <- function(x) {
-  # get absolute coordinates of the grob
-  x_pt <- convertX(x$x, "pt", valueOnly = TRUE)
-  y_pt <- convertY(x$y, "pt", valueOnly = TRUE)
-
-  if (!is.null(x$width)) {
-    width_pt <- convertWidth(x$width, "pt", valueOnly = TRUE)
-  } else {
-    width_pt <- 0
-  }
-
-  bl_calc_layout(x$vbox, width_pt, 0)
-
-  w <- bl_box_width(x$vbox)
-  h <- bl_box_height(x$vbox)
-  grob <- bl_render(x$vbox, x_pt, y_pt)
-
-  x$grobwidth <- unit(w, "pt")
-  x$grobheight <- unit(h, "pt")
-
-  setChildren(x, gList(grob))
-}
-
-#' @export
-heightDetails.rich_text_grob <- function(x) {
-  if (is.null(x$grobheight)) {
-    x <- makeContent(x)
-  }
-  x$grobheight
-}
-
-#' @export
-widthDetails.rich_text_grob <- function(x) {
-  if (is.null(x$grobwidth)) {
-    x <- makeContent(x)
-  }
-  x$grobwidth
-}
-
-#' @export
-ascentDetails.rich_text_grob <- function(x) {
-  heightDetails(x)
-}
-
-#' @export
-descentDetails.rich_text_grob <- function(x) {
-  unit(0, "pt")
 }
---
+++ R/text-box-grob.R
@@ -0,0 +1,93 @@
+#' Text box grob
+#'
+#' @param text Character vector containing markdown/html string to draw.
+#' @param width,height Unit objects specifying width and height of the
+#'   grob; height is currently ignored.
+#' @param x,y Unit objects specifying the location of the reference point.
+#' @param hjust,vjust Numerical values specifying the location of the grob
+#'   relative to the reference point.
+#' @param gp Other graphical parameters for drawing
+#' @param use_markdown Should the `text` input be treated as markdown?
+#' @examples
+#' library(grid)
+#' grid.newpage()
+#' grid.draw(text_box_grob("Some text **in bold.**"))
+#' @export
+text_box_grob <- function(text, width = NULL, height = NULL,
+                           x = unit(0.5, "npc"), y = unit(0.5, "npc"),
+                           hjust = 0.5, vjust = 0.5, gp = gpar(), use_markdown = TRUE) {
+  if (use_markdown) {
+    text <- markdown::markdownToHTML(text = text, options = c("use_xhtml", "fragment_only"))
+  }
+  doctree <- read_html(text)
+
+  drawing_context <- setup_context(gp = gp)
+  boxlist <- process_tags(xml2::as_list(doctree)$html$body, drawing_context)
+  if (is.null(width)) {
+    vbox <- bl_make_vbox(boxlist, width_pt = 0, hjust = hjust, vjust = vjust, width_policy = "native")
+  } else {
+    vbox <- bl_make_vbox(boxlist, width_pt = 100, hjust = hjust, vjust = vjust, width_policy = "relative")
+  }
+
+  gTree(
+    width = width,
+    height = height,
+    x = x,
+    y = y,
+    hjust = hjust,
+    vjust = vjust,
+    vbox = vbox,
+    gp = gp,
+    cl = "rich_text_grob"
+  )
+}
+
+#' @export
+makeContent.text__box_grob <- function(x) {
+  # get absolute coordinates of the grob
+  x_pt <- convertX(x$x, "pt", valueOnly = TRUE)
+  y_pt <- convertY(x$y, "pt", valueOnly = TRUE)
+
+  if (!is.null(x$width)) {
+    width_pt <- convertWidth(x$width, "pt", valueOnly = TRUE)
+  } else {
+    width_pt <- 0
+  }
+
+  bl_calc_layout(x$vbox, width_pt, 0)
+
+  w <- bl_box_width(x$vbox)
+  h <- bl_box_height(x$vbox)
+  grob <- bl_render(x$vbox, x_pt, y_pt)
+
+  x$grobwidth <- unit(w, "pt")
+  x$grobheight <- unit(h, "pt")
+
+  setChildren(x, gList(grob))
+}
+
+#' @export
+heightDetails.text_box_grob <- function(x) {
+  if (is.null(x$grobheight)) {
+    x <- makeContent(x)
+  }
+  x$grobheight
+}
+
+#' @export
+widthDetails.text_box_grob <- function(x) {
+  if (is.null(x$grobwidth)) {
+    x <- makeContent(x)
+  }
+  x$grobwidth
+}
+
+#' @export
+ascentDetails.text_box_grob <- function(x) {
+  heightDetails(x)
+}
+
+#' @export
+descentDetails.text_box_grob <- function(x) {
+  unit(0, "pt")
+}
