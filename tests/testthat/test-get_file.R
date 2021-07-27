823f71b3201a8bd54c1453c0f65a3e16c5f0127d
mrcaseb <roemerle@gmail.com>
2021-07-27 22:09:16 +0200

    add test for `get_file()`

---
+++ tests/testthat/test-get_file.R
@@ -0,0 +1,8 @@
+test_that("get_file works", {
+  # get_file returns raw data if it's an url and a character path if it's
+  # a local path. That's why we test it with the function read_image that calls it
+  expect_identical(
+    read_image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/20140427Iris_versicolor1.jpg/320px-20140427Iris_versicolor1.jpg"),
+    read_image("../figs/test_image.jpg")
+  )
+})
