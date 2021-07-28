73c41fcf3e3bf8cbaa201644a12a4040960fbdc7
mrcaseb <roemerle@gmail.com>
2021-07-28 09:27:01 +0200

    use new test image

--- tests/testthat/test-get_file.R
+++ tests/testthat/test-get_file.R
@@ -4,7 +4,7 @@ test_that("get_file works", {
   # get_file returns raw data if it's an url and a character path if it's
   # a local path. That's why we test it with the function read_image that calls it
   expect_identical(
-    read_image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/20140427Iris_versicolor1.jpg/320px-20140427Iris_versicolor1.jpg"),
-    read_image("../figs/test_image.jpg")
+    read_image("https://upload.wikimedia.org/wikipedia/commons/6/62/Biedronka.drs.png"),
+    read_image("../figs/test_image.png")
   )
 })
